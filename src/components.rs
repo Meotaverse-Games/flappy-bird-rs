cf62a2fef8d32e63e6f78f1dc04c876162d8402d
Riyansu <kenKEN.DEM@gmail.com>
2025-07-26 12:22:56 +0900

    add objects

---
+++ src/components.rs
@@ -0,0 +1,29 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+pub struct Background;
+
+// components.rs
+#[derive(Component)]
+pub struct Ground;
+
+#[derive(Component)]
+pub struct GetReady;
+
+#[derive(Component)]
+pub struct GameOver;
+
+#[derive(Component)]
+pub struct Tap;
+
+#[derive(Component)]
+pub struct Score;
+
+#[derive(Component)]
+pub struct Bird;
+
+#[derive(Component)]
+pub struct TopPipe;
+
+#[derive(Component)]
+pub struct BottomPipe;
